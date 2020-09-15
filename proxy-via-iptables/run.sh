22474eca82403e576b8f004df5c6e989648c1dd9
s-nt-s <santos82h@gmail.com>
2020-09-15 11:47:46 +0200

    pasar cadena de conexión por parametro

--- proxy-via-iptables/run.sh
+++ proxy-via-iptables/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Falta parámetro para conectar a la base de datos. Ejemplo":
+  echo "$0 \"hosts db user pass\""
+  exit 1
+fi
+
 NAME="proxy-via-iptables"
 if ! docker image inspect "${NAME}:latest" >/dev/null 2>&1; then
   docker build  -t "${NAME}" .
 fi
-docker run --privileged "${NAME}:latest" 2>&1 | tee -a log.log
+docker run --privileged -e "DB=${1}" "${NAME}:latest" 2>&1 | tee -a log.log
